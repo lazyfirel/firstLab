2d2c1b2e88afe1b40678fcd33ac4a1667c48f5a9
lazyfirel <lazyfirelvrn@gmail.com>
2018-09-28 03:40:29 +0300

    masterChangesToMergeWithTemporary

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello WorldD!");
 	System.out.println("ThirdCommit!!!!!");
+        System.out.println("masterBranch");// where is temporary output? merge must be!
     }
 }
